8fca5ea2ae4e518f75530308f18c20929854f8f4
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-04-07 17:15:30 +0900

    add: Dependency Injection

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
+    constructor(private boardsService: BoardsService){}
     
 }
