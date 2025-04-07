528df07dac9b1f72e4690c7e37075a59ec9129d9
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-04-07 17:08:54 +0900

    add: board Service 추가

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+    
+}
