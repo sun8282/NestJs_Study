9f9715da185a6e3e00cf09570e425a6b4eebec75
hasunha1324@gmail.com <109707901+sun8282@users.noreply.github.com>
2025-04-09 18:58:12 +0900

    add: createDto

---
+++ dist/boards/dto/create-board.dto.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateBoardDto = void 0;
+class CreateBoardDto {
+    title;
+    description;
+}
+exports.CreateBoardDto = CreateBoardDto;
+//# sourceMappingURL=create-board.dto.js.map
